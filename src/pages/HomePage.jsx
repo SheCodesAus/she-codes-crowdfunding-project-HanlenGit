00d5d2ac96b3fb773deb016f28f975142bb00893
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-04-23 11:17:11 +0800

    Update Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // Component
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 // data
-import { allProjects } from "../data";
+// import { allProjects } from "../data";
 
 function HomePage() {
+// States (set the project list with our fake data for now) - useState preserves some values between function calls. 
+    const [projectList, setProjectList] = useState([]);
+
+// Action & helpers (runs the code after the render) useEffect is a hook. Telling the component to do something after render. 
+    useEffect(() => {
+        // go get the stuff in this URL, come back later
+        fetch(`${process.env.REACT_APP_API_URL}projects`)
+        .then((results) => {
+        // sure when you get the data, convert into json,
+        return results.json();
+        })
+        .then((data) => {
+        // when you do this set the project list. 
+        setProjectList(data);
+        });
+    }, []);
+
     return (
         <div id="project-list">
-            {allProjects.map((projectData, key) => {
-                return <ProjectCard key={key} projectData={projectData} />;
-             })}
+        {projectList.map((projectData, key) => {
+        return <ProjectCard key={key} projectData={projectData} />;
+        })}
         </div>
     );
 }
 export default HomePage;
+
+// To use an analogy to demonstrate async programming, consider the action of baking a cake. This action will be represented by a thread that is executing several steps (or tasks), as illustrated in code below. This code is serviceable, and you will still have a yummy cake once the method is done executing
