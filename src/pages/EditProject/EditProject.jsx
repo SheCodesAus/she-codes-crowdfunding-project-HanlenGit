9021dde26cefe16747ebedab879ad16be121a4a4
HanlenGit <94447216+HanlenGit@users.noreply.github.com>
2022-05-14 10:26:24 +0800

    Update EditProject.jsx

--- src/pages/EditProject/EditProject.jsx
+++ src/pages/EditProject/EditProject.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-// import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import EditProjectForm from "../../components/EditProjectForm/EditProjectForm";
 
 
 function EditProjectPage(){
+
+        // State
+        const [projectData, setProjectData] = useState();
+    
+        //Hooks
+        const { id } = useParams();
+    
+    
+        // network in use effect
+        useEffect(() => {
+    
+            // fetch project info
+            fetch(`${process.env.REACT_APP_API_URL}projects/${id}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then((project) => {
+            setProjectData(project);
+            });
+    
+        }, []);
+    
+        if (!projectData) {
+            return <h1>Loading...</h1>
+        }
 
    return (
         <div>
